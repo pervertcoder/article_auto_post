0a042140af402cfb1a5ddb08d6a1d13a7581d00d
Pervertcoder <lance60702@gmail.com>
2026-07-11 19:54:09 +0800

    refactor(meta_APIs): using requests to call meta APIs and put the meta APIs calling logic into two functions
    Put meta API calling into the function in order to use its return value in another py file.
    The process will be
        1. My own APIs get the data from frontend, and put them into cleaned function which is from data_cleaning.py
        2. After being cleaned, the data will be put into function which is for calling meta APIs which is from meta_APIs.py
        3. The return data will be transformmed via cleaned function and then transformmed into my own APIs
    
    Using the third party library "requests" to call APIs

--- backend/meta_api.py
+++ backend/meta_api.py
@@ -1,29 +1,29 @@
 import requests
-from config import thread_user_id, meta_thread_api_token
 
-user_id = thread_user_id
-access_token = meta_thread_api_token
+def making_post_container(url:str, payload:dict) -> dict:
+    result = {}
+    post_container_response = requests.post(url, payload)
+    data = post_container_response.json()
+    container_id = data["id"]
+    if container_id:
+        result["state"] = True
+        result["container_id"] = container_id
+    else:
+        result["state"] = False
+        result["container_id"] = None
 
-url1 = f"https://graph.threads.net/v1.0/{user_id}/threads"
-url2 = f"https://graph.threads.net/v1.0/{user_id}/threads_publish"
+    return result
 
-payload1 = {}
-# payload and api format example for url1
+def making_real_text_post(url:str, payload:dict) -> dict:
+    result = {}
+    post_response = requests.post(url, payload)
+    data = post_response.json()
+    thread_media_id = data["id"]
+    if thread_media_id:
+        result["state"] = True
+        result["thread_media_id"] = thread_media_id
+    else:
+        result["state"] = False
+        result["thread_media_id"] = None
 
-# curl -i -X POST \
-#   -d "media_type=IMAGE" \
-#   -d "image_url=<IMAGE_URL>" \
-#   -d "text=<TEXT>" \
-#   -d "access_token=<ACCESS_TOKEN>" \
-# "https://graph.threads.net/v1.0/<THREADS_USER_ID>/threads"
-
-
-payload2 = {}
-# payload and api format example for url2
-
-# curl -i -X POST \
-#   -d "creation_id=<MEDIA_CONTAINER_ID>" \
-#   -d "access_token=<ACCESS_TOKEN>" \
-# "https://graph.threads.net/v1.0/<THREADS_USER_ID>/threads_publish"
-
-response = requests.post(url1, json=payload1)
+    return result
