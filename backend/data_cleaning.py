ee978cbcea94134d0693ad7f7112ff5c95ab3f2a
Pervertcoder <lance60702@gmail.com>
2026-08-02 12:16:17 +0800

    chore(meta_APIs, data_cleaning): change the location (from data_cleaning.py to meta_api.py) of the instruction of meta APIs and design cleaning function for test
    Originally, the instruction was put in data_cleaning.py because meta apis were called right after cleaning the data.
    However, this structure is better: Data_cleaning function and APIs are called in the router. In this way, the developer
    can see how to deal with the data in the router which is clearer to see.
    For cleaning function, this is just for test, and will be completed later
    
    ref: meta_api.py, data_cleaning.py

--- backend/data_cleaning.py
+++ backend/data_cleaning.py
@@ -1,30 +1,4 @@
-from config import thread_user_id, meta_thread_api_token
-from meta_api import making_post_container, making_real_text_post
-
-user_id = thread_user_id
-access_token = meta_thread_api_token
-
-url1 = f"https://graph.threads.net/v1.0/{user_id}/threads"
-url2 = f"https://graph.threads.net/v1.0/{user_id}/threads_publish"
-
-payload1 = {}
-# payload and api format example for url1
-
-# curl -i -X POST \
-#   -d "media_type=IMAGE" \
-#   -d "image_url=<IMAGE_URL>" \
-#   -d "text=<TEXT>" \
-#   -d "access_token=<ACCESS_TOKEN>" \
-# "https://graph.threads.net/v1.0/<THREADS_USER_ID>/threads"
-
-
-payload2 = {}
-# payload and api format example for url2
-
-# curl -i -X POST \
-#   -d "creation_id=<MEDIA_CONTAINER_ID>" \
-#   -d "access_token=<ACCESS_TOKEN>" \
-# "https://graph.threads.net/v1.0/<THREADS_USER_ID>/threads_publish"
-
 def cleaning_data(data:str):
-    ...
+    data_length = len(data)
+    if data_length < 50:
+        return data
--- backend/meta_api.py
+++ backend/meta_api.py
@@ -1,5 +1,34 @@
 import requests
 
+from config import thread_user_id, meta_thread_api_token
+
+
+user_id = thread_user_id
+access_token = meta_thread_api_token
+
+url1 = f"https://graph.threads.net/v1.0/{user_id}/threads"
+url2 = f"https://graph.threads.net/v1.0/{user_id}/threads_publish"
+
+payload1 = {}
+# payload and api format example for url1
+
+# curl -i -X POST \
+#   -d "media_type=IMAGE" \
+#   -d "image_url=<IMAGE_URL>" \
+#   -d "text=<TEXT>" \
+#   -d "access_token=<ACCESS_TOKEN>" \
+# "https://graph.threads.net/v1.0/<THREADS_USER_ID>/threads"
+
+
+payload2 = {}
+# payload and api format example for url2
+
+# curl -i -X POST \
+#   -d "creation_id=<MEDIA_CONTAINER_ID>" \
+#   -d "access_token=<ACCESS_TOKEN>" \
+# "https://graph.threads.net/v1.0/<THREADS_USER_ID>/threads_publish"
+
+
 def making_post_container(url:str, payload:dict) -> dict:
     result = {}
     post_container_response = requests.post(url, payload)
