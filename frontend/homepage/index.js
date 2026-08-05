12d0fa9853c416bee004b59fdda265da5bc117f1
Willy Huang <willy@WillydeMacBook-Air.local>
2026-08-05 11:59:32 +0800

    feat(index.js): add timer to call health check api every 10 seconds and write the comments to help identifing the different blocks of the file
    Firstly, try to call health check API every 10 seconds to check if backend app has any issue
    Secondly, define different blocks with the comments to help identifing different code block to raise efficiency of the development.
    Making good habit!!
    
    ref: index.js

--- frontend/homepage/index.js
+++ frontend/homepage/index.js
@@ -1,9 +1,21 @@
 "use strict";
 
+// DOM變數
 const buttonIg = document.getElementById("ig");
 const buttonThreads = document.getElementById("threads");
 const post = document.getElementById("post");
 
+// 呼叫API的函式跟參數
+
+// 健康測試API
+const checkHealthAPIUrl = "http://127.0.0.1:8000/health";
+const checkHealth = async function (url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+
+// 用健康測試API測試按鈕
 const testAPIUrl = "http://127.0.0.1:8000/health";
 const apiTest = async function (url) {
   const response = await fetch(url);
@@ -13,6 +25,7 @@ const apiTest = async function (url) {
   post.value = "test";
 };
 
+// Threads post API
 const threadsAPIUrl = "http://127.0.0.1:8000/poster/threads";
 const threadApi = async function (url) {
   // 這邊需要考慮空白
@@ -33,6 +46,7 @@ const threadApi = async function (url) {
   console.log(data);
 };
 
+// 按鈕事件
 buttonIg.addEventListener("click", () => {
   apiTest(testAPIUrl);
 });
@@ -41,3 +55,8 @@ buttonThreads.addEventListener("click", () => {
   // console.log("test");
   threadApi(threadsAPIUrl);
 });
+
+// 健康測試（間隔10秒）
+setInterval(() => {
+  checkHealth(checkHealthAPIUrl)
+}, 10000);
