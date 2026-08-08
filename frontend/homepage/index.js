1bbbe86d6078ae75d44badf8655ea36e53915cad
Pervertcoder <lance60702@gmail.com>
2026-08-09 01:36:34 +0800

    feat(frontend folder and spec_brainstorming): add the feature of adding new textarea and deleting new textarea
    Cleaning the data at the backend will cause the interruption of meaning which
    will have trouble on understanding the post.
    The solution is cleaning the data at frontend. Dividing the data by putting in
    different textarea and the function of backend is calling meta APIs
    
    ref: frontend folder, spec_brainstorming.txt

--- frontend/homepage/index.js
+++ frontend/homepage/index.js
@@ -5,6 +5,63 @@ const buttonIg = document.getElementById("ig");
 const buttonThreads = document.getElementById("threads");
 const post = document.getElementById("post");
 
+const buttonAddParagraph = document.getElementById("add");
+const buttonDeleteParagraph = document.getElementById("delete");
+
+const parentSection = document.querySelector(".workarea");
+
+// 畫面功能函式
+let dataId = 0;
+const alphabet = [
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const addParagraphFun = function () {
+  const addedParagraph = document.createElement("textarea");
+  addedParagraph.name = "post";
+  addedParagraph.id = "post" + "_" + alphabet[dataId];
+  addedParagraph.dataset.id = dataId + 1;
+  addedParagraph.rows = 10;
+  addedParagraph.cols = 30;
+
+  const addParagraph = parentSection.appendChild(addedParagraph);
+  dataId++;
+};
+const deleteParagraphFun = function () {
+  if (parentSection.childElementCount >= 4) {
+    parentSection.removeChild(parentSection.lastElementChild);
+    dataId--;
+  } else {
+    console.log("no more added paragraph to delete");
+    return;
+  }
+};
+
 // 呼叫API的函式跟參數
 
 // 健康測試API
@@ -39,7 +96,7 @@ const threadApi = async function (url) {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ content: post.value }),
+    body: JSON.stringify({ content: no_space_string }),
   });
   const data = await response.json();
 
@@ -54,6 +111,14 @@ buttonIg.addEventListener("click", () => {
 buttonThreads.addEventListener("click", () => {
   // console.log("test");
   threadApi(threadsAPIUrl);
+});
+
+buttonAddParagraph.addEventListener("click", () => {
+  addParagraphFun();
+});
+
+buttonDeleteParagraph.addEventListener("click", () => {
+  deleteParagraphFun();
 });
 
 // 健康測試（間隔1分鐘）
